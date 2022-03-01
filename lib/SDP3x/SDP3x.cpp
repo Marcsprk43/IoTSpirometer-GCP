40dbb8404476d5cb4e9b9f36606ea85e83e850d7
gerrievanzyl <43932356+Marcsprk43@users.noreply.github.com>
2022-03-01 12:27:36 -0500

    first commit

---
+++ lib/SDP3x/SDP3x.cpp
@@ -0,0 +1,350 @@
+/*
+    SDP3x.cpp - Library for the SDP31 and SDP32 digital pressure sensors produced by Sensirion.
+    Created by Bryan T. Meyers, February 14, 2017.
+
+    Copyright (c) 2018 Bryan T. Meyers
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+    and associated documentation files (the "Software"), to deal in the Software without
+    restriction, including without limitation the rights to use, copy, modify, merge, publish,
+    distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all copies or
+    substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+    BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+    DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include "SDP3x.h"
+
+using namespace SDP3X;
+
+/*  Send a write command
+
+    @param cmd - the two byte command to send
+    @returns true iff all ACKs received
+*/
+bool SDP3x::writeCommand(const uint8_t cmd[2]) {
+    size_t written;
+    uint8_t status;
+
+  Serial.print("Writing Command Addr: 0x");
+  Serial.print(this->addr, HEX);
+  Serial.print(" CMD0: 0x");
+  Serial.print(cmd[0],HEX);
+  Serial.print(" CMD1: 0x");
+  Serial.println(cmd[1], HEX);
+
+  Wire.beginTransmission(this->addr);
+  delay(20);
+  
+    written = Wire.write(cmd, 2);
+  delay(20);
+    status  = Wire.endTransmission();
+  
+  Serial.print("Written = ");
+  Serial.print(written);
+  Serial.print(" Status =  ");
+  Serial.println(status,HEX);
+
+    return (status == 0) && (written == 2);
+}
+
+/*  Read data back from the device
+
+    @param words - the number of words to read
+    @returns true iff all words read and CRC passed
+*/
+bool SDP3x::readData(uint8_t words) {
+    size_t read;
+    uint8_t crc = 0xFF;
+    uint8_t *next;
+    bool success = true;
+    // Clear buffer
+    for (next = &this->buffer[12]; next > this->buffer; next--) {
+        *next = 0;
+    }
+    // Each word is two bytes plus a CRC byte, ergo 3 bytes per word
+    read = Wire.requestFrom(this->addr, (uint8_t)(words * 3));
+
+    /*  We should have read the requested number of bytes.
+        If not, we need to clear the bytes read anyways.
+    */
+    if (read != 3 * words) {
+        success = false;
+    }
+    /*  Calculate CRC while reading bytes
+
+        Adapted from:
+        http://www.sunshine2k.de/articles/coding/crc/understanding_crc.html
+    */
+    next = this->buffer;
+    for (; read > 0; read--) {
+        // Read next available byte
+        *next = Wire.read();
+        // Every third byte
+        if ((read % 3) == 1) {
+            // Check CRC byte
+            success = success && (crc == *next);
+            crc     = 0xFF;
+        } else {
+            // Update CRC
+            crc = CRC_LUT[crc ^ *next];
+            // Go to next byte
+            next++;
+        }
+    }
+    return success;
+}
+
+/*  Constructor
+
+    @param addr - the Address value for I2C
+    @param comp - the Temperature Compensation Mode (Mass Flow or Differential Pressure)
+    @returns a new SDP3X as configured
+*/
+SDP3x::SDP3x(const uint8_t addr, TempCompensation comp) {
+    this->addr = addr;
+    this->comp = comp;
+}
+
+/*  Finish Initializing the sensor object
+
+    @returns true, iff everything went correctly
+*/
+bool SDP3x::begin() {
+    uint32_t modelNumber;
+  Serial.println("Reading product number...");
+  Serial.print("I2C pins - SDA: ");
+  Serial.print(SDA);
+  Serial.print(" SCL: ");
+  Serial.println(SCL);
+
+
+    if (!readProductID(&modelNumber, NULL)) {
+        return false;
+    }
+    Serial.print("Product Number: ");
+    Serial.println(modelNumber, HEX);
+  
+    switch (modelNumber) {
+    case SDP31_PID:
+        this->number = SDP31;
+        return true;
+    case SDP32_PID:
+        this->number = SDP32;
+        return true;
+    case SDP33_PID:
+        this->number = SDP33;
+        return true;
+
+    default:
+        /* do nothing for now */
+        return false;
+    }
+}
+
+/*  Begin taking continuous readings
+
+    @param averaging - average samples until read occurs, otherwise read last value only
+    @returns true, iff everything went correctly
+*/
+bool SDP3x::startContinuous(bool averaging) {
+    switch (this->comp) {
+    case MassFlow:
+        if (averaging) {
+            return writeCommand(StartContMassFlowAvg);
+        } else {
+            return writeCommand(StartContMassFlow);
+        }
+    case DiffPressure:
+        if (averaging) {
+            return writeCommand(StartContDiffPressureAvg);
+        } else {
+            return writeCommand(StartContDiffPressure);
+        }
+    default:
+        return false;
+    }
+}
+
+/*  Disable continuous measurements
+
+    This may be useful to conserve power when sampling all the time is no longer necessary.
+    @returns true, iff everything went correctly
+*/
+bool SDP3x::stopContinuous() {
+    return writeCommand(StopCont);
+}
+
+/*  Start a one-shot reading
+
+    @param averaging - average samples until read occurs, otherwise read last value only
+    @returns true, iff everything went correctly
+*/
+bool SDP3x::triggerMeasurement(bool stretching) {
+    switch (this->comp) {
+    case MassFlow:
+        if (stretching) {
+            return writeCommand(TrigMassFlowStretch);
+        } else {
+            return writeCommand(TrigMassFlow);
+        }
+    case DiffPressure:
+        if (stretching) {
+            return writeCommand(TrigDiffPressureStretch);
+        } else {
+            return writeCommand(TrigDiffPressure);
+        }
+    default:
+        return false;
+    }
+}
+
+/*  Get a pending reading.
+
+    This may be used periodically or in a call-back when monitoring interrupts.
+
+    Both "temp" and "scale" should be left NULL if not used. This will reduce read times.
+    @param pressure - a pointer to store the raw pressue value
+    @param temp  - a pointer to store the raw temperature value
+    @param scale - a pointer to store the pressure scaling factor
+    @returns the raw pressure reading (no scaling)
+    @returns true, iff everything went correctly
+*/
+bool SDP3x::readMeasurement(int16_t *pressure, int16_t *temp, int16_t *scale) {
+    uint8_t words = 1;
+    if (scale != NULL) {
+        words = 3;
+    } else if (temp != NULL) {
+        words = 2;
+    }
+    if (!readData(words)) {
+        return false;
+    }
+    /*  Data Format:
+        | Byte  |  0  |  1  |  2  |  3  |  4  |  5  |
+        | Value | pressure  | temp      | scale     |
+    */
+    switch (words) {
+    case 3:
+        if (scale != NULL) {
+            *scale <<= 8;
+            *scale |= (int16_t)this->buffer[4];
+            *scale <<= 8;
+            *scale |= (int16_t)this->buffer[5];
+        }
+    case 2:
+        if (temp != NULL) {
+            *temp <<= 8;
+            *temp |= (int16_t)this->buffer[2];
+            *temp <<= 8;
+            *temp |= (int16_t)this->buffer[3];
+        }
+    case 1:
+        if (pressure != NULL) {
+            *pressure <<= 8;
+            *pressure |= (int16_t)this->buffer[0];
+            *pressure <<= 8;
+            *pressure |= (int16_t)this->buffer[1];
+        }
+    }
+    return true;
+}
+
+/*  Read back the sensor's internal information
+
+    If a serial number is not needed, "serial" should be set to NULL. This will reduce read
+   times.
+    @param pid     - a pointer to store the 32-bit product ID
+    @param serial  - if not null, a pointer to store the 64-bit manufacturer serial number
+    @returns true, iff everything went correctly
+*/
+bool SDP3x::readProductID(uint32_t *pid, uint64_t *serial) {
+    uint8_t words = 2;
+    if (serial != NULL) {
+        words = 6;
+    }
+  Serial.println("Sending read instruction1");
+    // Send first command
+    if (!writeCommand(ReadInfo1)) {
+        return false;
+    }
+  Serial.println("Sending read instruction2");
+
+    // Send decond command
+    if (!writeCommand(ReadInfo2)) {
+        return false;
+    }
+  Serial.println("Sending read data");
+
+    // Read back required data
+    if (!readData(words)) {
+        return false;
+    }
+    /*  Data Format:
+        | Byte  | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 |
+        | Value | pid           | serial                          |
+    */
+    switch (words) {
+    case 6:
+        // "Parse" serial number
+        if (serial != NULL) {
+            for (words = 0; words < 8; words++) {
+                *serial <<= 8;
+                *serial |= (uint64_t)this->buffer[words + 4];
+            }
+        }
+    case 2:
+        // "Parse" product identifer
+        if (pid != NULL) {
+            for (words = 0; words < 4; words++) {
+                *pid <<= 8;
+                *pid |= (uint32_t)this->buffer[words];
+            }
+        }
+    }
+    return true;
+}
+
+/*  Reset the device to default settings
+
+    WARNING: This will reset all other I2C devices that support it.
+    @returns true, iff everything went correctly
+*/
+bool SDP3x::reset() {
+    size_t written = 0;
+    uint8_t status;
+    Wire.beginTransmission(SoftReset[0]);
+    written = Wire.write(SoftReset[1]);
+    status  = Wire.endTransmission();
+    return (written == 1) && (status == 0);
+}
+
+/*  Get the Pressure Scale for this sensor
+
+    @returns scale in units of 1/Pa
+*/
+uint8_t SDP3x::getPressureScale() {
+    switch (this->number) {
+    case SDP31:
+        return SDP31_DiffScale;
+    case SDP32:
+        return SDP32_DiffScale;
+    default:
+        return 1;
+    }
+}
+
+/*  Get the Temperature Scale for this sensor
+
+    @returns scale in units of 1/C
+*/
+uint8_t SDP3x::getTemperatureScale() {
+    return SDP3X_TempScale;
+}
---
+++ src/main.cpp
@@ -0,0 +1,499 @@
+
+/***************************************************
+ * 
+ *                 IoT Spirometer
+ * 
+ *                       by
+ *                  Marc van Zyl
+ * 
+ * *************************************************/
+
+// Global config defines
+#define IOT_ACTIVE 0
+
+
+#include <Arduino.h>
+#include <ctype.h>
+
+#include "SDP3x.h"
+#include "U8g2lib.h"   // LCD Screen includes
+#include "U8x8lib.h"  // LCD Screen font includes
+
+
+
+void initTime(){
+  time_t epochTime;
+  configTime(0,0, "pool.ntp.org", "time.nist.gov");
+
+  while(true){
+    epochTime = time(NULL);
+
+    if (epochTime == 0){
+      Serial.println("Fetching NTP epoch time failed! Retry in 2s");
+      delay(2000);
+    } else {
+      Serial.printf("Fetched NTP time: %lu.\r\n", epochTime);
+      break;
+    }
+  }
+}
+
+// LCD Screen
+U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, SCL, SDA, U8X8_PIN_NONE);
+
+
+// THese are the states
+enum states {
+  STATE_IDLE,
+  STATE_W_INHALE_B,
+  STATE_R_INHALE_B,
+  STATE_W_EXHALE,
+  STATE_R_EXHALE,
+  STATE_W_INHALE,
+  STATE_R_INHALE,
+  STATE_PRINT,
+  STATE_IOT_UPLOAD,
+  STATE_ERROR
+};
+
+float simpleVol;
+float simpleVelocity;
+float lastSimpleVelocity;
+float maxSimpleVol;
+
+#define SIMPLE_VOL_THRESHOLD 0.05  // This is the trial accuracy threshold 
+
+int state;
+
+// constants for the pressures thresholds between states
+// if the pressure goes above or below these trigger the state transition
+#define EXHALE_PRESSURE_THRESHOLD 40
+#define INHALE_PRESSURE_THRESHOLD 40
+#define INHALE_PRESSURE_THRESHOLD_B 50
+
+// Constants to decide on timeouts
+#define MAX_EXHALE_TIME 5000  // 5 seconds
+#define MAX_INHALE_TIME 5000  // 5 seconds
+
+// Other constants
+#define DIR_EXHALE 1.0
+#define DIR_INHALE -1.0
+#define D1 0.025  // Diameter of the venturi tubes
+#define D2 0.015  // Diameter of the venturi throat
+#define A1 3.14159*D1*D1/4.  // area of the venturi tubes
+#define A2 3.14159*D2*D2/4.  // area of the venturi throat
+#define AIR_DENSITY 1.225  // density at sea level at 20 deg C 1.225kg/m3
+#define M3_TO_LITRES 1000.0  // cubic meters to litres conversion
+#define A2D_UNITS_TO_PRESSURE 1/20.0 // from the pressure sensor datasheet
+
+// arrays to store 5 seconds of the pressure for inhale and EXHALE_PRESSURE_THRESHOLD
+int16_t exhalePressure[500];
+int16_t inhalePressure[500];
+
+using namespace SDP3X;
+
+SDP3x sensor(Address1, MassFlow); //create the pressure sensor
+
+#define PERIOD 10 // this is the period in ms of the pressure sensor readings
+#define DT PERIOD/1000.0 // This is the time of each loop in seconds
+
+unsigned long next_tick;
+unsigned long start_time;
+
+// This draws the cool flying triangles
+void u8g2_triangle(uint8_t a){
+  uint16_t offset = a;
+  u8g2.drawStr( 0, 0, "drawTriangle");
+  u8g2.drawTriangle(14,7, 45,30, 10,40);
+  u8g2.drawTriangle(14+offset,7-offset, 45+offset,30-offset, 57+offset,10-offset); 
+  u8g2.drawTriangle(57+offset*2,10, 45+offset*2,30, 86+offset*2,53); 
+  u8g2.drawTriangle(10+offset,40+offset, 45+offset,30+offset, 86+offset,53+offset);
+
+}
+
+// This is from the u8g2 Graphics Test program - prepares the screen
+void u8g2_prepare(void) {
+  u8g2.setFont(u8g2_font_6x10_tf);
+  u8g2.setFontRefHeightExtendedText();
+  u8g2.setDrawColor(1);
+  u8g2.setFontPosTop();
+  u8g2.setFontDirection(0);
+}
+
+// function to clear the screen and prepare it for printing
+void LCD_Clear(void){
+  u8g2.clearBuffer();
+  u8g2_prepare();
+}
+
+// function to print a string to the screen
+void LCD_Print(int x, int y, const char *str1){
+  u8g2.drawStr(x, y, str1);
+  u8g2.sendBuffer();
+}
+
+// Function to print a floating point number
+void LCD_Print_Float(int x, int y, float f){
+  String str = String(f);
+  char chars[str.length()];
+  str.toCharArray(chars, str.length());
+  u8g2.drawStr(x, y, chars);
+  u8g2.sendBuffer();
+}
+
+// function to play a tone for .2 seconds
+
+#define TONE_PIN 4
+
+void play_tone(int duration){
+  digitalWrite(TONE_PIN, HIGH);
+  delay(duration);
+  digitalWrite(TONE_PIN, LOW);
+}
+
+/*****************************************************************/
+/*************************      SETUP      ***********************/
+/*****************************************************************/
+
+void setup() {
+  // this setup code runs once only
+  
+  // start the screen
+  u8g2.begin();
+  u8g2.setFlipMode(true);
+
+  // Start the serial connection for debugging.
+  Serial.begin(115200);
+  Serial.flush();
+
+  // Start the communication with the pressure sensor
+  Wire.begin();
+  while(!sensor.begin()){
+    Serial.print("Board not initialized!");
+    delay(1000);
+  }
+
+  // Start the sensor in "continuous mode" with averaging enabled
+  // this means the sensor gets a new reading every 1ms but I only
+  // read it every 10ms.  It averages the readings in between.
+  sensor.startContinuous(true);
+
+  // This prints the start screen 
+  LCD_Clear();
+  LCD_Print(0,0,"An IoT Spirometer");
+  delay(1000);
+  LCD_Clear();
+  LCD_Print(0,0,"by Marc van Zyl");
+  delay(1000);
+  for (int i=0; i<8; i++){
+    u8g2_prepare();
+    u8g2_triangle(i);
+    delay(100);
+  }
+
+  // Setup the buzzer
+  pinMode(TONE_PIN, OUTPUT);
+  digitalWrite(TONE_PIN, LOW);
+  play_tone(1000);
+  
+  // Setup the button
+  pinMode(2, INPUT);
+  digitalWrite(2, HIGH);  // this activates the internal pull-up resistor
+
+  // Print the instructions screen
+  LCD_Clear();
+  LCD_Print(0,0, "Ready to read");
+  LCD_Print(0,10, "Bring the");
+  LCD_Print(0,20, "Spirometer to");
+  LCD_Print(0,30, "your lips and");
+  LCD_Print(0, 40, "Press the button");
+  LCD_Print(0, 50, "and inhale");
+  delay(1000);
+
+  // This sets the timer
+  next_tick = millis() + PERIOD;
+  start_time = next_tick;
+}
+
+
+// Global variables
+bool status;
+float f_pressure;
+float cum_vol;
+float flow_rate;
+float last_flow_rate;
+float direction;
+int16_t pressure;
+
+unsigned long readFlowStartTime;
+unsigned int exhaleCount;
+unsigned int inhaleCount;
+
+// Function to read the pressure sensor and to do the calculations
+
+void readSensor(void){
+  status = sensor.readMeasurement(&pressure, NULL, NULL);
+  f_pressure = -1.0*pressure*A2D_UNITS_TO_PRESSURE;
+  pressure *= -1;
+  float velocity = direction*sqrt(-2.0*f_pressure/(AIR_DENSITY*(pow((A2/A1),2.0) - 1.0)));
+  flow_rate = velocity*A2*M3_TO_LITRES;
+  cum_vol += DT*((flow_rate + last_flow_rate)/2.0);
+  last_flow_rate = flow_rate;
+}
+
+/*****************************************************************/
+/*************************      LOOP       ***********************/
+/*****************************************************************/
+
+// The main loop that runs continuously
+void loop() {
+
+  // wait for the next tick to start the cycle
+  while (millis() < next_tick){
+    delay(1);
+  }
+
+  next_tick += PERIOD;  // set the next tick time
+
+  readSensor();  // read the pressure sensor
+
+  switch(state){
+    case STATE_IDLE:
+      // *** state execution commands
+
+      // *** state transition - check for button press
+      if (digitalRead(2) == LOW){  // Check for button press
+        // next state initialization
+        LCD_Clear();
+        LCD_Print(0,0,"Waiting for inhale");
+        Serial.println("Starting data acqusition");
+        Serial.println("Waiting for initial inhale");
+        readFlowStartTime = millis();
+        // initialize the variables for volumne Check
+        simpleVol = 0.0;
+        simpleVelocity = 0.0;
+        lastSimpleVelocity = 0.0;
+        maxSimpleVol = 0.0;
+        play_tone(300);  // play a short tone 
+        state = STATE_W_INHALE_B;
+      }
+      break;
+    case STATE_W_INHALE_B:
+      // *** state execution commands
+
+      // *** state transition - wait for the inhale to begin
+      if (pressure > INHALE_PRESSURE_THRESHOLD_B){ // advance the state if true
+        readFlowStartTime = millis();
+        LCD_Clear();
+        LCD_Print(0,0, "Initial inhaling");
+        Serial.println("Initial inhaling");
+        state = STATE_R_INHALE_B; // advance the state to read inhale B
+      }
+      // *** state transition - check for timeout after MAX_INHALE_TIME
+      if (millis() > readFlowStartTime + MAX_INHALE_TIME){
+        LCD_Clear();
+        LCD_Print(0,0, "Timed out!");
+        Serial.println("Timed out on state STATE_W_INHALE_B");
+        delay(1000);
+        state = STATE_ERROR;  // go to the error state
+      }
+      break;
+
+    case STATE_R_INHALE_B:
+      // *** state execution commands
+      
+      // *** state transition - check for end of inhale
+      if  (pressure < INHALE_PRESSURE_THRESHOLD-5){ // check if the inhale has ended
+        LCD_Clear();
+        LCD_Print(0,0,"Waiting for exhale");
+        Serial.println("Found end of initial inhale....waiting for exhale");
+        readFlowStartTime = millis();
+        play_tone(300);
+        state = STATE_W_EXHALE;
+      }
+      if (millis() > readFlowStartTime + 5000){
+        LCD_Clear();
+        LCD_Print(0,0,"Timed out");
+        Serial.println("Timed out on state STATE_R_INHALE_B");
+        delay(1000);
+        state = STATE_ERROR;
+      }
+      break;
+    case STATE_W_EXHALE:
+      // *** state execution commands
+
+      // *** state transition -  check for begin of exhale
+      if (pressure > EXHALE_PRESSURE_THRESHOLD){ // check if the exhale has started
+        LCD_Clear();
+        LCD_Print(0,0,"Found Exhale");
+        Serial.println("Found exhale");
+        exhaleCount = 0;
+        readFlowStartTime = millis();
+        lastSimpleVelocity = 0;
+        state = STATE_R_EXHALE;
+      }
+      // *** state transition - timeout
+      if (millis() > readFlowStartTime + 5000){
+        LCD_Clear();
+        LCD_Print(0,0,"Timed out");
+        Serial.println("Timed out on state STATE_W_EXHALE");
+        state = STATE_ERROR;
+      }
+      break;
+    case STATE_R_EXHALE:
+      // *** state execution commands
+      exhalePressure[exhaleCount] = pressure; // save the pressure in the array
+      
+      if (exhaleCount < 500-1)  // this protects against writing more than the allocated memory
+        exhaleCount++;
+      simpleVelocity = sqrt( (float)abs(pressure));
+      simpleVol += (lastSimpleVelocity + simpleVelocity)/2;
+      lastSimpleVelocity = simpleVelocity;
+
+      // *** state transition - check for end of exhale
+      if (pressure < EXHALE_PRESSURE_THRESHOLD-5){  // this means the exhale is over
+        LCD_Clear();
+        LCD_Print(0,0,"Waiting for inhale");
+        Serial.println("Exhale done....Waiting for inhale");
+        maxSimpleVol = simpleVol;
+        readFlowStartTime = millis();
+        play_tone(300);
+        state = STATE_W_INHALE;
+      }
+      // *** state transition - timeout
+      if (millis() > readFlowStartTime + 5000){
+        LCD_Clear();
+        LCD_Print(0,0,"Timed out");
+        Serial.println("Timed out on state STATE_R_EXHALE");
+        delay(1000);
+        state = STATE_ERROR;
+      }
+      break;
+
+    case STATE_W_INHALE:
+      // *** state execution commands
+
+      // *** state transition - check for start of inhale
+      if (pressure > INHALE_PRESSURE_THRESHOLD){  // the inhale has started
+        inhaleCount = 0;
+        readFlowStartTime = millis();
+        lastSimpleVelocity = 0;
+        LCD_Clear();
+        LCD_Print(0,0,"Found inhale");
+        Serial.println("Found inhale");
+        state = STATE_R_INHALE;
+      }
+      // *** state transition - timeout
+      if (millis() > readFlowStartTime + 5000){
+        LCD_Clear();
+        LCD_Print(0,0,"Timed out");
+        Serial.println("Timed out on state STATE_W_INHALE");
+        delay(1000);
+        state = STATE_ERROR;
+      }
+      break;
+    case STATE_R_INHALE:
+      // *** state execution commands
+      inhalePressure[inhaleCount] = pressure; // save the pressure in the array
+
+      if (inhaleCount < 500-1)
+        inhaleCount++;
+      simpleVelocity = sqrt( (float)abs(pressure));
+      simpleVol -= (lastSimpleVelocity + simpleVelocity)/2;
+      lastSimpleVelocity = simpleVelocity;
+
+      // *** state transition - check for end of inhale
+      if (pressure < INHALE_PRESSURE_THRESHOLD-5){ // check for end of inhale
+        play_tone(300);  // play three tones to signal end of cycle
+        delay(300);
+        play_tone(300);
+        delay(300);
+        play_tone(300);
+        delay(300);
+        state = STATE_PRINT;
+      }
+
+      // *** state transition - timeout
+      if (millis() > readFlowStartTime + 5000){
+        LCD_Clear();
+        LCD_Print(0,0,"Timed out");
+        Serial.println("Timed out on state STATE_R_INHALE");
+        delay(1000);
+        state = STATE_ERROR;
+      }
+      break;
+
+    case STATE_PRINT:  
+      // *** state execution commands
+      // first print the results to serial 
+      Serial.println("Starting Print");
+      for (int i=0; i<exhaleCount; i++)  // print the exhale data
+        Serial.println(exhalePressure[i], DEC);
+      for (int i=0; i<inhaleCount; i++)  // print the inhale data
+        Serial.println(inhalePressure[i], DEC);
+
+      // Check if the exhale - inhale cycle was successful  
+      if ( abs(simpleVol/maxSimpleVol) > SIMPLE_VOL_THRESHOLD ){
+        LCD_Clear();
+        LCD_Print(0,0,"Failed blow");
+        LCD_Print_Float(0,10, simpleVol/maxSimpleVol);
+        delay(3000);
+      }
+      // print results to the LCD
+      LCD_Clear();
+      LCD_Print(0,0,"Max Vol: ");
+      LCD_Print_Float(45,0, maxSimpleVol);
+      // print results to the serial port
+      Serial.print("maxSimpleVol: ");
+      Serial.println(maxSimpleVol);
+      Serial.print("simpleVol: ");
+      Serial.println(simpleVol);
+      Serial.print("Percent error:");
+      Serial.println(simpleVol/maxSimpleVol);
+
+      // *** state transition - to STATE_IOT_UPLOAD
+      state = STATE_IOT_UPLOAD;
+      break;
+
+    case STATE_ERROR:
+      // *** state execution commands
+      LCD_Clear();
+      LCD_Print(0,0,"Return to Idle");
+      Serial.println("In Error state .... Returning to idle");
+
+      // *** state transition - to STATE_IOT_UPLOAD
+      state = STATE_IOT_UPLOAD;
+      break;
+    
+    case STATE_IOT_UPLOAD:
+      // *** state execution commands
+      LCD_Clear();
+      LCD_Print(0,0,"Uploading to cloud");
+      Serial.println("In STATE_IOT_UPLOAD state .... ");
+
+      if (IOT_ACTIVE){
+        // insert the IoT upload here
+
+      } else {
+        LCD_Print(0,10,"IoT Upload-False");
+        Serial.println("Upload to cloud bypassed IOT_ACTIVE is false ");
+        Serial.println("Returning to STATE_IDLE");
+
+        delay(2000);
+        
+      }
+      // *** state transition - to STATE_IDLE
+      state = STATE_IDLE;
+      break;
+
+    default:
+      // *** state execution commands
+      LCD_Clear();
+      LCD_Print(0,0,"Error default state");
+      Serial.println("Error in default state - return to state STATE_IDLE");
+      
+      // *** state transition - to STATE_IDLE
+      state = STATE_IDLE;
+      break; 
+  }
+}
